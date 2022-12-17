a3a73bc90aa346aec16dc79f7c41af9c150282e2
kawa-nobu <44832116+kawa-nobu@users.noreply.github.com>
2022-12-17 20:54:59 +0900

    削除対象の検出方法変更
    従来はスクロールイベント検出で消していたが、今度はMutationObserverに検出させるように変更。
    これで、スクロールの有無にかかわらずすぐに消える。

--- content.js
+++ content.js
@@ -1,10 +1,17 @@
 console.log("Clean-DLSite is Working!");
-let work_index;
-window.addEventListener('scroll', function() {
-    if(document.getElementsByClassName('_filter').length != 0){
-        let rem_elem = document.getElementsByClassName('_filter');
-        for(let i=0;rem_elem.length>i;i++){
-            rem_elem[i].parentElement.parentElement.remove();
-        }
+const target_elem = document.getElementById("wrapper");
+function run(){
+    let rem_elem = document.getElementsByClassName('_filter');
+    for(let i=0;rem_elem.length>i;i++){
+        rem_elem[i].parentElement.parentElement.remove();
     }
+};
+const observer = new MutationObserver(run)
+observer.observe(target_elem,{
+    childList: true,
+    attributes: true,
+    characterData: true,
+    subtree: true,
+    attributeOldValue: true,
+    characterDataOldValue: true
 });
